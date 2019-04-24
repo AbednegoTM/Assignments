38c751050c1218fbfd7d9f48d23292962867a21e
Nchimunya Banda <47819858+Nchimzy708@users.noreply.github.com>
2019-04-24 02:15:13 +0200

    react js assignment submitted

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+ <header className="App-header">
+      <h1>Web Projects</h1>
+      </header>
+
+
+        <main className="App-header1">
+      <p>Building the web with react</p>
+        </main>
+
+      <footer className="App-header2">
+      <p>Read.Learn.Do</p>
+      </footer>
+
+      </div>
+      
+    );
+
+    
+  }
+}
+
+export default App;
